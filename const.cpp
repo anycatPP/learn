8140eae22a8273959e156dada63e07cd8c195795
Aniket singh <64830202+anycatPP@users.noreply.github.com>
2020-09-30 13:22:15 +0530

    Update const.cpp

--- const.cpp
+++ const.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 int main()
 {	//this will give error because const can be changed not even get compiled
-	const char *str="cpp";
+	const char *str="cpp"; //string literal example its constant cant be changed unlike string 
+	but it can be swapped with another string literal
+	const char* ptr ="sadf";
+	ptr=str; // true 
 
 	char str[3]="fas";
 	return 0;
