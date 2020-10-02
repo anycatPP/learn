cfc2408b85b901686ec6a539b2e42a71ccb38ebe
anycatPP <aniket.singh.esc@gmail.com>
2020-10-02 21:57:11 +0530

    naya chod

---
+++ bada lauda loge/lauda loge/Source.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+int main()
+{
+	std::cout << "the fuck is going on";
+	std::cin.get();
+}
--- linked_list/linked_list.cpp
+++ linked_list/linked_list.cpp
@@ -2,6 +2,8 @@
 
 int main()
 {
+    int a=10;
     std::cout << "Hello World!\n";
+    std::cin.get();
 }
 
