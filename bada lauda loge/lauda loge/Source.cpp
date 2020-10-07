7407342b8e3ace5e7e86328d873b9c22c5f01214
anycatPP <aniket.singh.esc@gmail.com>
2020-10-07 14:31:57 +0530

    Sychronized folders

--- bada lauda loge/lauda loge/Source.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-int main()
-{
-	std::cout << "the fuck is going on";
-	std::cin.get();
-}
--- const.cpp
+++ const.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 using namespace std;
 int main()
-{	//this will give error because const can be changed not even get compiled
+{	//this will give error because const cant be changed not even get compiled
 	const char *str="cpp"; //string literal example its constant cant be changed unlike string 
-	but it can be swapped with another string literal
+	//but it can be swapped with another string literal
 	const char* ptr ="sadf";
 	ptr=str; // true 
 
