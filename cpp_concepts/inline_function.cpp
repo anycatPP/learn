f07c6527642ceb70098a4fe037d00206dac6628b
anycatPP <aniket.singh.esc@gmail.com>
2020-10-07 15:29:41 +0530

    inline function for small function to avoid function call overhaead

---
+++ cpp_concepts/inline_function.cpp
@@ -0,0 +1,62 @@
+/*inline tabhi use karo function me jab function chota ho aur tumko function call overhead se bachna ho 
+ *
+ * sahi cheez hai par dhyaan se use karna 
+ * yaha se deep me padh lo
+ *https://www.geeksforgeeks.org/inline-functions-cpp/*/
+#include <iostream> 
+using namespace std; 
+class operation 
+{ 
+	int a,b,add,sub,mul; 
+	float div; 
+public: 
+	void get(); 
+	void sum(); 
+	void difference(); 
+	void product(); 
+	void division(); 
+}; 
+inline void operation :: get() 
+{ 
+	cout << "Enter first value:"; 
+	cin >> a; 
+	cout << "Enter second value:"; 
+	cin >> b; 
+} 
+
+inline void operation :: sum() 
+{ 
+	add = a+b; 
+	cout << "Addition of two numbers: " << a+b << "\n"; 
+} 
+
+inline void operation :: difference() 
+{ 
+	sub = a-b; 
+	cout << "Difference of two numbers: " << a-b << "\n"; 
+} 
+
+inline void operation :: product() 
+{ 
+	mul = a*b; 
+	cout << "Product of two numbers: " << a*b << "\n"; 
+} 
+
+inline void operation ::division() 
+{ 
+	div=a/b; 
+	cout<<"Division of two numbers: "<<a/b<<"\n" ; 
+} 
+
+int main() 
+{ 
+	cout << "Program using inline function\n"; 
+	operation s; 
+	s.get(); 
+	s.sum(); 
+	s.difference(); 
+	s.product(); 
+	s.division(); 
+	return 0; 
+}
+
