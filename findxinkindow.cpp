4a8b5e0d9e9f808f92f407419dac4b27d21509e1
anycatPP <aniket.singh.esc@gmail.com>
2021-03-24 13:41:20 +0530

    bhej do

---
+++ findxinkindow.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+bool findxinkindow(int arr[],int x, int k,int n)
+{
+	for(int i=0;i<=k;i++)
+		for(int j=0;j<=n;j++)
+			sum=sum+arr[j];
+
+int main()
+{
+	int arr[]={1,2,3,4,5,6,7,8,9};
+	int x=3,k=3;
+	int n=sizeof(arr)/sizeof(arr[0]);
+	if(findxinkindowsize(arr,x,k,n);
+			cout<<"yes"<<endl;
+			else 
+			cout<<"no"<<endl;
+			return 0;
+			}
+
