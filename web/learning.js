e8b08d4cb33f690075beeafae72060f808b18944
anycatPP <aniket.singh.esc@gmail.com>
2021-03-25 23:11:16 +0530

    react app

--- web/learning.js
+++ web/learning.js
@@ -1,1 +1,8 @@
-//array in js
+const animal = ['uddeshya', 'aur pata  nhi'];
+console.log(animal);
+
+const count = animal.unshift('aniket');
+console.log(animal);
+const coun = animal.shift('adding something');
+console.log(animal);
+console.log(animal.pop());
